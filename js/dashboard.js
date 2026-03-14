ad325a8dcccde53a5d186f0a59021de42a28d434
gowthamreddy23 <gowthamreddy.study@gmail.com>
2026-03-14 22:12:02 +0530

    modifications

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,11 +1,18 @@
 if(localStorage.getItem("isLoggedIn") !== "true"){
 window.location.href = "../login.html";
 }
+
 function logout(){
 
 localStorage.removeItem("isLoggedIn");
 localStorage.removeItem("username");
+window.location.href = "../login.html";
+}
 
-window.location.href="../login.html";
-
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebar");
+if(menuBtn){
+menuBtn.addEventListener("click", () => {
+sidebar.classList.toggle("-translate-x-full");
+});
 }
